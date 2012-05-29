f64450942ab93c94b7a4ade6a19775c2de4e110d
Mael Clerambault <mael@clerambault.fr>
2012-05-29 23:50:14 +0200

    Use daemon

--- lib/generic_unix_server.rb
+++ lib/generic_unix_server.rb
@@ -1,36 +1,27 @@
 require 'socket'
+require_relative 'daemon'
 
 class GenericUnixServer
+  MAX_CMD_LEN=1024
+  class Error < StandardError; end
+  class CmdToLong < StandardError; end
+  include Daemon
+
   def self.def_cmd(cmd, &blk); define_method("process_" + cmd.to_s, &blk) end
   
   def_cmd(:CMD)  { |*args| "OK " + @known_commands.keys.sort.join(" ") }
   def_cmd(:HELP) { |*args| "OK Usage: CMD param1 param2 ..." }
   def_cmd(:ECHO) { |*args| "OK " + args.join(" ") }
 
-  def initialize(socket_file)
-    @socket_file = socket_file
+  def start!
     File.delete(@socket_file) if File.exists?(@socket_file)
-    @known_commands = Hash.new(:unknown)
-    methods.each {|m| @known_commands[m.to_s.sub(/^process_/, '')] = m if m =~ /^process_/ }
-  end
-  
-  def process(line)
-    cmd, *args = line.split(" ")
-    __send__(@known_commands[cmd], *args)
-  rescue => e
-    p e
-    "NOK Internal Error"
-  end
-  
-  def unknown(*args); "NOK Unknown Command" end
-
-  def start
     @server = UNIXServer.open(@socket_file)
     File.chmod(0666, @socket_file)
       
     while c = @server.accept
       begin
-      while l = c.gets
+      while l = c.gets(MAX_CMD_LEN)
+        raise CmdToLong if l.size >= MAX_CMD_LEN
         c.puts process(l.strip).to_s
       end
       rescue => e
@@ -39,21 +30,42 @@ class GenericUnixServer
         c.close
       end
     end
-  end
-  
-  def stop
+  ensure
     @server.close
     File.delete(@socket_file)
   end
+  
+  private
+  def initialize(socket_file)
+    @socket_file = File.expand_path(socket_file)
+    @known_commands = Hash.new(:unknown)
+    methods.each {|m| @known_commands[m.to_s.sub(/^process_/, '')] = m if m =~ /^process_/ }
+  end
+  
+  def process(line)
+    cmd, *args = line.split(" ")
+    __send__(@known_commands[cmd], *args)
+  rescue Error => e
+    "NOK " + e.to_s
+  rescue => e
+    p e
+    "NOK Internal Error"
+  end
+  
+  def unknown(*args); "NOK Unknown Command" end
 end
 
 class GenericUnixClient
+  class Error < StandardError; end
   def initialize(s); @socket_file = s end
   
   def send_cmd(cmd, *params)
     UNIXSocket.open(@socket_file) {|f|
       f.puts params.unshift(cmd).join(" ")
-      status, *response = f.gets.split(" ")
+      l = f.gets
+      raise Error if l.nil?
+      status, *response = l.split(" ")
+      raise Error.new(response.join(" ")) if status != "OK"
       response
    }
   end
